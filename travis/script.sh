f50ba19466a8f5846fe56d421a3180e11acf5aba
Adam Bergmark <adam@bergmark.nl>
2017-07-09 17:58:20 +0200

    Build benchmarks with --fast on travis to speed up the build (note that we don't run the benchmarks)

--- travis/script.sh
+++ travis/script.sh
@@ -6,9 +6,13 @@ case $BUILD in
   stack)
     stack build --test --haddock --no-terminal
     ;;
+  bench)
+      cabal update
+      cabal install . benchmarks/ --disable-opt
+    ;;
   cabal)
     if [ -f configure.ac ]; then autoreconf -i; fi
-    cabal configure --enable-tests --enable-benchmarks -v2  # -v2 provides useful information for debugging
+    cabal configure --enable-tests --enable-benchmarks --disable-opt -v2  # -v2 provides useful information for debugging
     cabal build   # this builds all libraries and executables (including tests/benchmarks)
     cabal test
     cabal sdist   # tests that a source-distribution can be generated
@@ -17,7 +21,7 @@ case $BUILD in
     # If there are no other `.tar.gz` files in `dist`, this can be even simpler:
     # `cabal install --force-reinstalls dist/*-*.tar.gz`
     SRC_TGZ=$(cabal info . | awk '{print $2;exit}').tar.gz &&
-      (cd dist && cabal install --force-reinstalls "$SRC_TGZ")
+      (cd dist && cabal install --force-reinstalls --disable-opt "$SRC_TGZ")
     ;;
   hlint)
     stack build --fast aeson --stack-yaml stack-lts8.yaml --system-ghc --no-terminal
